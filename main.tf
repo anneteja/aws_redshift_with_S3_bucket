b53c2b54e8bf3cd3c733c2dc813ea79ae0035216
anneteja <31939828+anneteja@users.noreply.github.com>
2018-04-03 13:37:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = "${aws_s3_bucket.bucket.id}"
 
-  policy = <<EOF
+  policy = <<-EOF
 {
   "Version": "2012-10-17",
   "Statement": [
